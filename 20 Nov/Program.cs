9608c617ff5475a6e3586c28ec7485128f857111
NouranOmar98 <norankinane@gmail.com>
2022-11-20 21:33:06 +0300

    Tasks

---
+++ 20 Nov/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Task1
+string Firstname = "Nouran";
+Console.WriteLine(Firstname);
+//Task2
+double myDoubleNum = 5.99D;
+Console.WriteLine(myDoubleNum);
+string fname = "Nouran";
+Console.WriteLine(fname);
+char firstchar = 'A';
+Console.WriteLine(firstchar);
+bool nouran = true;
+Console.WriteLine(nouran);
+int Number = 15;
+Console.WriteLine(Number);
+const int myNum = 25;
+Console.WriteLine(myNum);
+//Task3
+string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+Console.WriteLine(cars[0]+" "+cars[1]+" " + cars[2]+" " + cars[3]);
+//Task4
+Console.WriteLine("Enter Your First Name");
+string firstname = Console.ReadLine();
+Console.WriteLine("Enter Your Last Name");
+string lastname = Console.ReadLine();
+Console.WriteLine("Enter Your Birth Year");
+string birthyear = Console.ReadLine();
+Console.WriteLine(firstname+" "+lastname+" "+birthyear);
+//Task5
+int[] numbers = {1,1,2,3,4,5,6,7,8,9};
+Console.WriteLine(numbers[0] + " " + numbers[1] + " " + numbers[2] + " " + numbers[3] + " " + numbers[4] + " " + numbers[5] + " " + numbers[6] + " " + numbers[7] + " " + numbers[8] + " " + numbers[9] + " ");
+//Task6
+int[] num ={2,5,8};
+Console.WriteLine(num[0] + num[1] + num[2]);
+
+
+
+
+
+
